2ea65b7a80c10a985a4cf6b84622a94882739954
Andrew Jarcho <andrew.jarcho@gmail.com>
2022-06-16 10:16:53 -0400

    Break out `call_db()`
    On branch lookup_others
    Changes to be committed:
        modified:   lookup.py

--- lookup.py
+++ lookup.py
@@ -18,6 +18,41 @@ def lookup():
     if not search_string:
         print('Goodbye!!!')
         return
+    call_db(search_string)
+
+
+def select_search():
+    selection = int(input('\tEnter 1 to search by song title,\n'
+                          '\tEnter 2 to search by band/leader name,\n'
+                          '\tEnter 0 to quit:\n').strip() or '0')
+    while not 0 <= selection < 3:
+        selection = int(input('\t').strip())
+    return selection
+
+
+def get_search_string(selection: int):
+    if not selection:
+        search_string = ''
+    elif selection == 1:
+        yes_no = None
+        while not yes_no or yes_no[0] not in 'ynq':
+            title = input('What song title do you want to look up? ')
+            search_string = cleanup_title_string(title).lower()
+            yes_no = input(f'I will search for \'{search_string}\', ok? [Y/n])').strip().lower()
+            if not yes_no:
+                yes_no = 'y'
+            elif yes_no == 'y':
+                pass
+            elif yes_no == 'n':
+                yes_no = None
+            elif yes_no == 'q':
+                search_string = ''
+    elif selection == 2:
+        search_string = 'N.Y.I.'
+    return search_string
+
+
+def call_db(search_string):
     conn = None
     try:
         params = config()
@@ -49,38 +84,6 @@ def lookup():
         if conn is not None:
             conn.close()
             print('db connection closed')
-
-
-def select_search():
-    selection = int(input('\tEnter 1 to search by song title,\n'
-                          '\tEnter 2 to search by band/leader name,\n'
-                          '\tEnter 0 to quit:\n').strip())
-    while not 0 <= selection < 3:
-        selection = int(input('\t').strip())
-    return selection
-
-
-def get_search_string(selection: int):
-    if not selection:
-        search_string = ''
-    elif selection == 1:
-        yes_no = None
-        while not yes_no or yes_no[0] not in 'ynq':
-            title = input('What song title do you want to look up? ')
-            search_string = cleanup_title_string(title).lower()
-            yes_no = input(f'I will search for \'{search_string}\', ok? [Y/n])').strip().lower()
-            if not yes_no:
-                yes_no = 'y'
-            elif yes_no == 'y':
-                pass
-            elif yes_no == 'n':
-                yes_no = None
-            elif yes_no == 'q':
-                search_string = ''
-    elif selection == 2:
-        search_string = 'N.Y.I.'
-    return search_string
-
 
 
 if __name__ == '__main__':
