fa1bf9821812f2b312c39ee5c95a0f02c1f483f4
Andrew Jarcho <andrew.jarcho@gmail.com>
2022-06-21 21:46:05 -0400

    Continue working on flask
    On branch add_flask
    Changes to be committed:
        modified:   app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 from markupsafe import escape
+import os
+from sqlalchemy import create_engine
+import urllib.parse
+
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kaZ00bie!do@localhost/tunes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.environ['SECRET_KEY']
+
 
 @app.route('/')
 def find_a_tune():
