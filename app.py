b51a7a12e1ba5907d0906fca9419ea09a2cb1aed
Andrew Jarcho <andrew.jarcho@gmail.com>
2022-06-25 01:07:11 -0400

    Get by partial title working
    Needs cleanup.
    Bug: 80-char output limit on tune names (look up 'summertime')
    Bug: doesn't correctly output non-ascii chars
    
    On branch add_flask
    Changes to be committed:
        modified:   app.py
        modified:   templates/base.html
        new file:   templates/by_partial.html
        modified:   templates/choose_query.html

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ def choose_query():
             return redirect(url_for('get_tune'))
         elif selected_value == '2':
             return redirect(url_for('by_band'))
+        elif selected_value == '3':
+            return redirect(url_for('by_partial'))
         else:
             return redirect(url_for('choose_query'))
     # radio = RadioField('Choose Query', choices=[
@@ -51,6 +53,10 @@ def show_releases():
         band_with_dashes = session['band'].replace(' ', '-')
         # cur.execute("SELECT discogs_release_string FROM tu_release WHERE discogs_release_string LIKE %s;", ('%' + session['band'] + '%',))
         cur.execute("SELECT discogs_release_string FROM tu_release WHERE discogs_release_string LIKE %s;", ('%' + band_with_dashes + '%',))
+    elif session['part'] is not None:
+        part_with_dashes = session['part'].replace(' ', '-')
+        # return f"session['part'] is {session['part']} -- part_with_dashes is {part_with_dashes}"
+        cur.execute("SELECT discogs_release_string, s.song_title FROM tu_release r JOIN tu_song_release sr ON r.release_id = sr.release_id JOIN tu_song s ON sr.song_id = s.song_id WHERE s.song_title LIKE %s;", ('%' + part_with_dashes + '%',))
     releases = cur.fetchall()
     cur.close()
     conn.close()
@@ -61,8 +67,9 @@ def show_releases():
 @app.route('/get', methods=["GET", "POST"])
 def get_tune():
     if request.method == "POST":
-        session['tune'] = request.form['tname']
         session['band'] = None
+        session['tune'] = request.form['tname']
+        session['part'] = None
         return redirect(url_for('show_releases'))
     return render_template('get_tune.html')
 
@@ -72,6 +79,17 @@ def by_band():
     if request.method == "POST":
         session['band'] = request.form['bname']
         session['tune'] = None
+        session['part'] = None
         return redirect(url_for('show_releases'))
     return render_template('by_band.html')
 
+
+@app.route('/part', methods=["GET", "POST"])
+def by_partial():
+    if request.method == "POST":
+        session['band'] = None
+        session['tune'] = None
+        session['part'] = request.form['pname']
+        # return session['part']
+        return redirect(url_for('show_releases'))
+    return render_template('by_partial.html')
