696b46cb8e72f444f22d114e95a2e24004b3879f
Andrew Jarcho <andrew.jarcho@gmail.com>
2022-06-25 19:09:45 -0400

    Remove sqlalchemy_01.py from index
    On branch add_flask_cleanup
    Changes to be committed:
        deleted:    sqlalchemy_01.py

--- sqlalchemy_01.py
+++
@@ -1,8 +0,0 @@
-import urllib.parse
-from sqlalchemy import create_engine
-
-escaped_pw = urllib.parse.quote_plus('kaZ00bie!do')
-# print(escaped_pw)
-
-engine = create_engine('postgresql+psycopg2://jazcap53:' + escaped_pw + '@localhost:5432/tunes')
-
