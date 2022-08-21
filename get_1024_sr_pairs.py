41eb35c9064060ce7d677a6ddbc90774187f7861
Andrew Jarcho <andrew.jarcho@gmail.com>
2022-08-20 20:47:09 -0400

    Add some docstrings
    Also set constant DB_BATCH_SIZE = 1024
    Improve type hints
    
    On branch add_mp3s_02
    Changes to be committed:
        modified:   get_1024_sr_pairs.py

--- get_1024_sr_pairs.py
+++ get_1024_sr_pairs.py
@@ -8,7 +8,11 @@ from back_end.connect import connect, get_release_list, do_close_routine
 from back_end.add_mp3s import get_mp3_set
 
 
+DB_BATCH_SIZE = 1024
+
+
 def get_song_release_ct():
+    """Get and return the number of tu_song_release items in the db"""
     query= "SELECT COUNT(*) FROM tu_song_release"
     with connect(silent=True) as conn:
         cur = conn.cursor()
@@ -19,7 +23,7 @@ def get_song_release_ct():
 
 
 def get_item_batches(directory: str ='/home/jazcap53/Music/misc_mp3s') -> set:
-    limit = 1024
+    limit = DB_BATCH_SIZE
     max_offset = get_song_release_ct()
     offset = 0
     batches_looked_at = 0
@@ -31,19 +35,30 @@ def get_item_batches(directory: str ='/home/jazcap53/Music/misc_mp3s') -> set:
              "LIMIT %s OFFSET %s")
     with connect(silent=True) as conn:
         while offset < max_offset:
-            all_items = get_one_batch(conn, query, limit, offset)
+            all_items = get_one_batch(conn, query, offset)
             if not all_items:
                 raise ValueError
             songs_found_in_db |= find_songs_in_db(all_items, song_and_file_names)
             offset += limit
             batches_looked_at += 1
-    print(f'Checked {batches_looked_at} batches of <= 1024 db items')
+    print(f'Checked {batches_looked_at} batches of <= {DB_BATCH_SIZE} db items')
     print(f'Found {len(songs_found_in_db)} songs from {directory} in db')
     print(*sorted(songs_found_in_db), sep='\n')
     return songs_found_in_db
 
 
-def find_songs_in_db(all_items: list, song_and_file_names: list):
+def find_songs_in_db(all_items: list[tuple], song_and_file_names: list[tuple]):
+    """
+    Return a set containing a (song name, file name) tuple for each
+    song from the file system that is found in the database.
+
+    parameter: all_items --
+               DB_BATCH_SIZE (song id, song title,
+                              release id, release string) tuples
+    parameter: song_and_file_names --
+               a (song name, file name) tuple for each song found in
+               the directory being examined
+    """
     song_names_only = [safn[0] for safn in song_and_file_names]
     songs_found_in_db = set()
     for item in all_items:
@@ -54,9 +69,10 @@ def find_songs_in_db(all_items: list, song_and_file_names: list):
     return songs_found_in_db
 
 
-def get_one_batch(conn, query, limit, offset) -> list:
+def get_one_batch(conn, query, offset) -> list:
+    """Get and return a batch of up to DB_BATCH_SIZE results from db connection"""
     with conn.cursor() as cur:
-        cur.execute(query, (limit, offset))
+        cur.execute(query, (DB_BATCH_SIZE, offset))
         all_items = sorted(cur.fetchall())
         cur.close()
     return all_items
